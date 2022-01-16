2bb938efced5716fa35f1d846801c4327b28bed2
Simon Burkhardt <simon@simonmartin.ch>
2022-01-16 18:41:09 +0100

    porting to C fix #1 partially fix #4 fix #2

--- src/easing.h
+++ src/easing.h
@@ -1,40 +1,34 @@
 #pragma once 
 
-enum easing_functions
-{
-	EaseInSine,
-	EaseOutSine,
-	EaseInOutSine,
-	EaseInQuad,
-	EaseOutQuad,
-	EaseInOutQuad,
-	EaseInCubic,
-	EaseOutCubic,
-	EaseInOutCubic,
-	EaseInQuart,
-	EaseOutQuart,
-	EaseInOutQuart,
-	EaseInQuint,
-	EaseOutQuint,
-	EaseInOutQuint,
-	EaseInExpo,
-	EaseOutExpo,
-	EaseInOutExpo,
-	EaseInCirc,
-	EaseOutCirc,
-	EaseInOutCirc,
-	EaseInBack,
-	EaseOutBack,
-	EaseInOutBack,
-	EaseInElastic,
-	EaseOutElastic,
-	EaseInOutElastic,
-	EaseInBounce,
-	EaseOutBounce,
-	EaseInOutBounce
-};
+float easeInSine( float t );
+float easeOutSine( float t );
+float easeInOutSine( float t );
+float easeInQuad( float t );
+float easeOutQuad( float t );
+float easeInOutQuad( float t );
+float easeInCubic( float t );
+float easeOutCubic( float t );
+float easeInOutCubic( float t );
+float easeInQuart( float t );
+float easeOutQuart( float t );
+float easeInOutQuart( float t );
+float easeInQuint( float t );
+float easeOutQuint( float t );
+float easeInOutQuint( float t );
+float easeInExpo( float t );
+float easeOutExpo( float t );
+float easeInOutExpo( float t );
+float easeInCirc( float t );
+float easeOutCirc( float t );
+float easeInOutCirc( float t );
+float easeInBack( float t );
+float easeOutBack( float t );
+float easeInOutBack( float t );
+float easeInElastic( float t );
+float easeOutElastic( float t );
+float easeInOutElastic( float t );
+float easeInBounce( float t );
+float easeOutBounce( float t );
+float easeInOutBounce( float t );
 
-typedef double(*easingFunction)(double);
-
-easingFunction getEasingFunction( easing_functions function );
 
